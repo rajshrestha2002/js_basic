c472c57d50aec0921d4268f99f155394cb6de71f
Raj Shrestha <rajshrestha2002@gmail.com>
2026-06-16 13:55:52 +0545

    add:datatypes

---
+++ 2datatypes.js
@@ -0,0 +1,33 @@
+// *data types
+
+// *number
+let a=23;
+//console.log(a);
+
+let b=45.6;
+//console.log(b);
+
+let c="hello";
+let user="Guest";
+let d="";
+//let d=' ';
+//console.log(`Hi ${user}`);
+//console.log(`${c}
+//    ${user}`);
+
+//*boolean
+let f=true;
+let g=false;
+
+//*undefined & not defined
+//leh h;
+//console.log(h);
+
+//null
+let i=null;
+console.log(i);
+
+//?non primitive
+//*object
+//*array
+//*function
