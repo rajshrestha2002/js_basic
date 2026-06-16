93ebd33437b5a2b0e99c8f31b6970fa71f2a1e43
Raj Shrestha <rajshrestha2002@gmail.com>
2026-06-16 13:54:35 +0545

    add:variables

---
+++ 1variables.js
@@ -0,0 +1,22 @@
+// var, let, const
+var a=10;
+a="Hello";
+// console.log(a);
+
+let b=100;
+// let b=566; &cannot declared second time
+b=566;
+// console.log(b);
+
+const c=123;
+// const c=231; error cannot be declared again
+// c=45;
+console.log(c);
+
+
+
+
+
+
+
+
