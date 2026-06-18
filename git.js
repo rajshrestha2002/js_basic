46de833c242f59cfcc2877d161b01a2667414337
Raj Shrestha <rajshrestha2002@gmail.com>
2026-06-18 12:06:02 +0545

    git.js

---
+++ git.js
@@ -0,0 +1,53 @@
+//git; github
+//*distrubuted VCS
+//github; gitlab; bitbucket
+//*repository
+//local
+//remote
+//branching
+//login //register //dashboard
+//*bug fix
+//*version
+//terminal commands
+//*git commands
+//*git config --global --list
+//*git config --global user.name "<user_name>"
+//*git config --global user.email "<user_email>"
+//*git config --global init.defaultbranch "main"
+//ls -a
+//git init // initialized empty git repository 
+// run in root folder of project for the first time
+
+//?git working flow
+//*working directory -> staging area -> local repo -> remote repo
+//changes -> ready state -> new version
+//?
+//*git status -> run in terminal
+//*git add <file_path>
+//*git add 1.variables.js //example
+//*git status
+//*git commit -m "<commit_message>"
+//*git commit -m "<add:variables>" //example
+// git add . //add current folder
+//?github
+//*git remote -v //lists repo
+//*git remote add origin <remote_repo_rul>
+//*git remote remove origin
+//*git push origin <branch_name> //from local git->github
+//*git push origin main //example
+//*git pull origin <branch_name> //from github->local git
+//? 
+//.gitignore //file //#//ignore.js
+// repeat the following process for every changes you made.
+//*git add .
+//*git commit -m "add:git remote comments"
+//*git push origin main
+//?branching
+//*git branch ->list all local branch
+//*git branch <branch_name> -> create new branch
+//*git switch <branch_name> -> switch between branches
+//?merge
+//*git merge <branch_name>
+//*git switch -c <branch_name> -> create & switch to new branch
+
+
